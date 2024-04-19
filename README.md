862d204fadaa80801368aa4717afc26093fd3f84
William Allen <williamallen933@gmail.com>
2024-04-19 17:30:32 -0400

    creating a 2day array to print matrix

---
+++ README.md
@@ -0,0 +1,2 @@
+Part one 
+creating this style of array was difficukt to understand its basically a 2 array that prints the matrix in rows 
---
+++ script.js
@@ -0,0 +1,13 @@
+//308.3 CSV data
+//ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+
+
+
+const matrix =[['ID', "Name", "Occupation", "Age"],
+               [42, "Bruce", "Knight", 41],
+               [57, "Bob", "Fry Cook", 19],
+               [63, "Blaine","Quiz Master",58],
+               [98, "Bill", "Doctor's Assistant", 26,]
+]
+
+console.log(matrix)
