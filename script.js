aaf0d01beddca51c724b5b8a58e3d2c60450768a
William Allen <williamallen933@gmail.com>
2024-04-19 23:47:12 -0400

    attempted to make a key and function failed attempt

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 //308.3 CSV data
+//Part 1 I would create multiple arrays 
 //ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
 
 
-
-const matrix =[['ID', "Name", "Occupation", "Age"],
+//Part 2
+let matrix = {data:[["ID", "Name", "Occupation", "Age"],
                [42, "Bruce", "Knight", 41],
                [57, "Bob", "Fry Cook", 19],
                [63, "Blaine","Quiz Master",58],
                [98, "Bill", "Doctor's Assistant", 26,]
-]
+]};
 
 console.log(matrix)
+////////////////
+
+///Part 3 nested for loops
+
+const filter=matrix.filter
+
+const items= matrix
+.filter(n => n >=  0)
+.map(n => ({value: n }))
+.filter (obj => (obj.value > 3))
+.map(obj => obj.value);
+ffilter =
+
+console.log(items)
+    
+
+
+
+
+
+
+
